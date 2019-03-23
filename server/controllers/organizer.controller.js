ad20dbd9906880745be151495acda0672eb4c3f1
alexfarra <farra.alexr@gmail.com>
2019-03-23 12:22:34 -0400

    create organizer schema and controller skeleton

---
+++ server/controllers/organizer.controller.js
@@ -0,0 +1,29 @@
+const organizerModel = require('../models/organizer.model');
+
+function create(req, res) {
+
+}
+
+function findAll(req, res) {
+
+}
+
+function findOne(req, res) {
+
+}
+
+function deleteOne(req, res) {
+
+}
+
+function updateOne(req, res) {
+
+}
+
+module.exports = {
+  create: create,
+  findAll: findAll,
+  findOne: findOne,
+  deleteOne: deleteOne,
+  updateOne: updateOne,
+};
