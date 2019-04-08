92e14231da30de46b7b65f236016002a9775d4f7
alexfarra <farra.alexr@gmail.com>
2019-04-07 21:25:39 -0400

    refactor to serve handlebars frontend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-const dbConfig = require('./database.config');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config');
@@ -13,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // serve directory that angular builds to
-app.use(express.static('../client/dist/registration-portal'));
+app.use(express.static('../client'));
 
 // use routes defined in routes.js
 app.use('/', routes);
